2451f9a96359d3fca4e30597e62a446b0e6088fa
Md. Al amin <alamincsecu@gmail.com>
2022-02-25 03:47:27 +0600

    ch#3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -18,35 +18,29 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome app"),
       ),
-      body: Center(
-        child: Container(
-          //color: Colors.red,
-          width: 100,
-          height: 100,
-          padding: const EdgeInsets.all(8.0),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
+      body: Container(
+        width: 500,
+        color: Colors.teal,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
               color: Colors.red,
-              borderRadius: BorderRadius.circular(10.0),
-              gradient: const LinearGradient(colors: [
-                Colors.pink,
-                Colors.yellow,
-              ]),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 10,
-                )
-              ]),
-          child: const Text(
-            "Hello flutter",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
             ),
-          ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            )
+          ],
         ),
       ),
     );
