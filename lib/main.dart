f602ef75e97ba05e17d081d374014a612b91f6aa
Md. Al amin <alamincsecu@gmail.com>
2022-02-25 01:36:57 +0600

    Ch#2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,37 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome app"),
       ),
-      body: Text("Hello flutter"),
+      body: Center(
+        child: Container(
+          //color: Colors.red,
+          width: 100,
+          height: 100,
+          padding: const EdgeInsets.all(8.0),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10.0),
+              gradient: const LinearGradient(colors: [
+                Colors.pink,
+                Colors.yellow,
+              ]),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 10,
+                )
+              ]),
+          child: const Text(
+            "Hello flutter",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
