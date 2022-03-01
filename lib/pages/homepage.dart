fc2939382ec3ed1baabe6bce17a80ca061144187
Md. Al amin <alamincsecu@gmail.com>
2022-03-01 08:55:15 +0600

    ch#5 -networking , listview, gridview

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
 import '../drawer.dart';
-import '../name_card.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,8 +11,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var myText = "Change My Name";
-  TextEditingController _nameController = TextEditingController();
+  // var myText = "Change My Name";
+  // TextEditingController _nameController = TextEditingController();
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var resData;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    var res = await http.get(Uri.parse((url)));
+    resData = jsonDecode(res.body);
+    setState(() {});
+    // print(resData);
+    // var res = await http.get(url);
+    //print(res.body);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,18 +45,22 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Awesome app"),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: NameCard(myText: myText, nameController: _nameController),
-          ),
-        ),
-      ),
+      body: resData != null
+          ? ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(resData[index]["title"]),
+                  subtitle: Text("ID: ${resData[index]["id"]}"),
+                  leading: Image.network(resData[index]["url"]),
+                );
+              },
+              itemCount: resData.length,
+            )
+          : Center(child: CircularProgressIndicator()),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          myText = _nameController.text;
-          setState(() {});
+          //myText = _nameController.text;
+          //setState(() {});
         },
         child: const Icon(Icons.send),
         hoverColor: Colors.purpleAccent, //Not working
