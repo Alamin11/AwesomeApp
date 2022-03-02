ad274afc14bc731dc132b14221fa93868f5ea4ea
Md. Al amin <alamincsecu@gmail.com>
2022-03-02 09:51:14 +0600

    button , stak , navigation

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,96 @@
+import 'package:awesome_app/pages/homepage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  final _userNameController = TextEditingController();
+  final _passController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login Page"),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            "assets/images/laptop.jpg",
+            fit: BoxFit.cover,
+            color: Colors.black.withOpacity(.7),
+            colorBlendMode: BlendMode.darken,
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: formKey,
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          TextFormField(
+                            controller: _userNameController,
+                            keyboardType: TextInputType.name,
+                            decoration: const InputDecoration(
+                              label: Text("UserName"),
+                              hintText: "Input username",
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          TextFormField(
+                            controller: _passController,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: const InputDecoration(
+                              label: Text("Password"),
+                              hintText: "Enter password",
+                            ),
+                            obscureText: true,
+                          ),
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          OutlinedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomePage()));
+                            },
+                            child: Text("Sign In"),
+                            style: ButtonStyle(
+                              elevation: MaterialStateProperty.all(0.0),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.grey),
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.orange),
+                              //textStyle: MaterialStateProperty.,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
